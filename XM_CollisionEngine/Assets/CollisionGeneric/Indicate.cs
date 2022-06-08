ec7739057a987e3ce412fbfa3ccce423f0e737b2
MUKILTEO\1552346 <jarzate26.ja10@gmail.com>
2022-06-08 12:37:45 -0700

    A

---
+++ XM_CollisionEngine/Assets/CollisionGeneric/Indicate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicate : MonoBehaviour
+{
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+    public void setValue(bool col)
+    {
+        if(col)
+        {
+            sr.color = new Color(0, 1, 0);
+        }else
+        {
+            sr.color = new Color(1, 0, 0);
+
+        }
+    }
+}
---
+++ XM_CollisionEngine/Assets/CollisionGeneric/VGDCollisionEngine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VGDCollisionEngine : MonoBehaviour
+{
+    public static bool PointPoint(float ax, float ay, float bx, float by)
+    {
+        if(ax==bx&&ay==by)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool PointPointBuffer(float ax, float ay, float bx, float by, float buffer)
+    {
+        float diffx = ax - bx;
+        float diffy = ay - by;
+        if (Mathf.Abs(diffx)<buffer&&Mathf.Abs(diffy)<buffer)
+        {
+            return true;
+        }
+        return false;
+    }
+}    
